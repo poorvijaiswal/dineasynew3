3de603b92a09fac52a5f57c4c118ed055cd3300a
Poorvi Jaiswal <jaiswalpoorvii0806@gmail.com>
2025-06-24 10:18:54 +0530

    origin changed

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'dineasynew5.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true
     }
